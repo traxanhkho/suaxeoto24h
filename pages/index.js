0ef8f69e61991c8fd748acfb49862ce116d1393f
datdang <toicho318@gmail.com>
2022-08-11 08:03:16 +0700

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Hello world i say !</h1>
+        <h1 className={styles.title}>i'll updated this home page . </h1>
 
         <p className={styles.description}>
           Get started by editing{" "}
